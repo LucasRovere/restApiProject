69d92577fa87f88afae0563d33037007fd0b2870
Lucas Rovere <thelucasrovere@gmail.com>
2019-02-15 16:52:28 -0200

    POST funcionando; POST gerando response corretamente

--- apiNoDataBase.py
+++ apiNoDataBase.py
@@ -2,6 +2,9 @@
 ######################## INIT #########################
 from flask import Flask
 from flask import request
+from flask import json
+from flask import make_response
+
 app = Flask(__name__)
 
 database = []
@@ -11,7 +14,19 @@ database = []
 # POST
 @app.route('/book/', methods=['POST'])
 def Post():
-	return '200: OK' + ' ' + str(request.data)
+	resource = json.loads(request.data)
+
+	# Verifica os dados obrigatórios
+	if ('title' not in resource):
+		return GenResponse(00, "Missing title")
+	if ('isbn' not in resource):
+		return GenResponse(400, "Missing isbn")
+
+	# Tenta adicionar no banco de dados
+	if (AddToDataBase(resource) == 1):
+		return GenResponse(201, "Resource Added!")
+	else:
+		return GenResponse(400, "Unknown Error")
 
 # GET
 @app.route('/books/', methods=['GET'])
@@ -19,3 +34,31 @@ def Get():
 	return '200: OK'
 
 #################### AUX FUNCTIONS ####################
+
+# Gera um arquivo do tipo response com os argumentos enviados
+def GenResponse(code, type):
+	the_response = make_response(type)
+	the_response.status_code = code
+
+	if(code == 201):
+		the_response.headers["code"] = "Created"
+	if(code == 204):
+		the_response.headers["code"] = "No Content"
+	if(code == 400):
+		the_response.headers["code"] = "Bad Request"
+
+	return the_response
+
+# Adiciona novo recurso na memória
+def AddToDataBase(data):
+	try:
+		database.append(data)
+	except:
+		return 0
+
+	return 1
+
+#the_response.code = "expired"
+#the_response.error_type = "expired"
+#the_response.status_code = 400
+#the_response._content = b'{ "key" : "a" }'
